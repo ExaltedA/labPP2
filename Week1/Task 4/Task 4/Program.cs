cc85624c9f5073a9cfde9ce91cdfc4089c0d7e3b
ExaltedA <aldie1741@gmail.com>
2019-02-15 01:04:57 +0600

    added task 4

---
+++ Week1/Task 4/Task 4/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_4
+{
+    class Program
+    {
+        static void Star(int a)
+        {
+            for (int i = 1; i <= a; i++)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write("[*]");
+                }
+                Console.WriteLine(" ");
+            }
+        }
+        static void Main(string[] args)
+        {
+            int a = Convert.ToInt32(Console.ReadLine());
+            Star(a);
+        }
+    }
+}
