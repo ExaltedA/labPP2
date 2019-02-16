0b8bcc2528658a30c83db5896c02c1b839edfe47
ExaltedA <aldie1741@gmail.com>
2019-02-16 07:15:18 +0600

    Task 4 lab 2 added

---
+++ Week2/Task 4/Task 4/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Task_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\Users\Exalted\Desktop\text12.txt";
+            string path1 = @"C:\Users\Exalted\Documents\copied.txt";
+            FileInfo t = new FileInfo(path);
+            if (File.Exists(path))
+            {
+                if (File.Exists(path1))
+                {
+                    t.CopyTo(path1, true);
+                    t.Delete();
+                }
+            }
+            else
+            {
+                StreamWriter r = new StreamWriter(path);
+                r.Close();
+
+                if (File.Exists(path1)==false)
+                {
+                    StreamWriter d = new StreamWriter(path1);
+                    d.Close();
+                }
+                t.CopyTo(path1, true);
+                t.Delete();
+
+            }
+            
+            
+           
+        }
+    }
+}
